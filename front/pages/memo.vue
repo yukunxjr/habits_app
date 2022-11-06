9bf37070f3d944192c88e1113940682d37437d93
yukunxjr <yukunxjr@yahoo.co.jp>
2022-11-06 20:51:56 +0900

    アカウントページの編集

--- front/pages/memo.vue
+++ front/pages/memo.vue
@@ -1,54 +1,94 @@
 <template>
-  <div id="clock">
-    <p class="date">{{ date }}</p>
-    <p class="time">{{ time }}</p>
-    <p class="text">DIGITAL CLOCK with Vue.js</p>
-  </div>
+  <v-card class="my-16 mx-auto pa-5" max-width="70%" outlined>
+    <v-card-title>TIMER</v-card-title>
+    <v-card-text>
+      <v-row class="text-center pa-15" justify="center">
+        <v-col cols="12" md="2">
+          <div class="text-h1">{{ hour }}</div>
+          <div class="text-h6">hour</div>
+        </v-col>
+        <v-col cols="12" md="1" class="text-h1">:</v-col>
+        <v-col cols="12" md="2">
+          <div class="text-h1">{{ min }}</div>
+          <div class="text-h6">min</div>
+        </v-col>
+        <v-col cols="12" md="1" class="text-h1">:</v-col>
+        <v-col cols="12" md="2">
+          <div class="text-h1">{{ sec }}</div>
+          <div class="text-h6">sec</div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <v-card-actions justify="center">
+      <v-btn @click="start" v-if="!timerOn">START</v-btn>
+      <v-btn @click="stop" v-if="timerOn">STOP</v-btn>
+      <v-btn @click="reset" v-if="!timerOn">RESET</v-btn>
+      <v-btn @click="overlay = !overlay" v-if="!timerOn">タイマー設定</v-btn>
+    </v-card-actions>
+    <v-overlay :z-index="zIndex" :value="overlay">
+      <v-btn class="white--text" color="teal" @click="overlay = false">
+        close
+      </v-btn>
+    </v-overlay>
+  </v-card>
 </template>
 
 <script>
-date: {
-}
+export default {
+  middleware: "auth",
+  data() {
+    let num = 0;
+    let zero = ("00" + num).slice(-2);
+    return {
+      hour: zero,
+      min: zero,
+      sec: zero,
+      timerOn: false,
+      timerObj: null,
+      overlay: false,
+      zIndex: 0,
+    };
+  },
+  methods: {
+    count: function () {
+      if (this.sec <= 0 && this.min >= 1) {
+        this.min--;
+        this.sec = 59;
+      } else if (this.sec <= 0 && this.min <= 0 && this.hour >= 1) {
+        this.hour--;
+        this.min = 59;
+        this.sec = 59;
+      } else if (this.sec <= 0 && this.min <= 0 && this.hour <= 0) {
+        this.complete();
+      } else {
+        this.sec--;
+      }
+    },
+
+    start: function () {
+      let self = this;
+      this.timerObj = setInterval(function () {
+        self.count();
+      }, 100);
+      this.timerOn = true;
+    },
+
+    stop: function () {
+      clearInterval(this.timerObj);
+      this.timerOn = false;
+    },
+
+    reset: function () {
+      this.hour = 0;
+      this.min = 0;
+      this.sec = 0;
+    },
+
+    timeSet: function () {},
+
+    complete: function () {
+      clearInterval(this.timerObj);
+    },
+  },
+};
 </script>
-
-<style lang="scss" scoped>
-html,
-body {
-  height: 100%;
-}
-body {
-  background: #0f3854;
-  background: radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
-  background-size: 100%;
-}
-p {
-  margin: 0;
-  padding: 0;
-}
-#clock {
-  font-family: "Share Tech Mono", monospace;
-  color: #ffffff;
-  text-align: center;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  color: #daf6ff;
-  text-shadow: 0 0 20px rgba(10, 175, 230, 1), 0 0 20px rgba(10, 175, 230, 0);
-
-  .time {
-    letter-spacing: 0.05em;
-    font-size: 80px;
-    padding: 5px 0;
-  }
-  .date {
-    letter-spacing: 0.1em;
-    font-size: 24px;
-  }
-  .text {
-    letter-spacing: 0.1em;
-    font-size: 12px;
-    padding: 20px 0 0;
-  }
-}
-</style>
