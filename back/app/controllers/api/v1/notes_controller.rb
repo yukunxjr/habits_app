aefac6da243055c429b3d48939b1f2710aec0932
yukunxjr <yukunxjr@yahoo.co.jp>
2022-11-09 05:41:48 +0900

    memoコントローラーをnoteコントローラーへ変更

---
+++ back/app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::NotesController < ApplicationController
+    before_action :set_note, only: %i[show destroy update]
+
+    def index
+        # notes = note.all.order(:id)
+        @notes = Note.all
+        render json: @notes
+    end
+
+    def show
+        render json: @note
+    end
+
+    def create
+        note = Note.new(note_params)
+        if note.save
+            render json: note
+        else
+            render json: note.errors
+        end
+    end
+
+    def update
+      if @note.update(note_params)
+        render json: @note
+      else
+        render json: @note.errors
+      end
+    end
+    
+    def destroy
+      if @note.destroy
+        render json: @note
+      else
+        render json: @note.errors
+      end
+    end
+    
+    private
+
+    def set_note
+      @note = Note.find(params[:id])
+    end
+  
+    def note_params
+      params.require(:note).permit(:content, :state)
+    end
+end
+
