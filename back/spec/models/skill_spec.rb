3e235414be4e697d55b7edd110c31a76f12a1c54
yukunxjr <yukunxjr@yahoo.co.jp>
2022-12-02 13:26:25 +0900

    skillモデルのテスト

--- back/spec/models/skill_spec.rb
+++ back/spec/models/skill_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(
+      name: "Test",
+      email: "test@example.com",
+      password: "password",
+    )
+  end
+  it "ユーザーIDがなければ無効な状態であること" do
+    skill = Skill.new(user_id: nil)
+    expect(skill.valid?).to eq(false)
+  end
+
+  it "名前が２０文字を超えれば無効な状態であること" do
+    skill = Skill.new(name: "a" * 21)
+    expect(skill.valid?).to eq(false)
+  end
 end
