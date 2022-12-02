cd62ccfcea36db154f64fafdbc480e26e459465f
yukunxjr <yukunxjr@yahoo.co.jp>
2022-12-02 13:47:59 +0900

    studyモデルのテスト

--- back/app/models/study.rb
+++ back/app/models/study.rb
@@ -4,5 +4,6 @@ class Study < ApplicationRecord
 
     validates :date, presence: true
     validates :skill_id, presence: true
+    validates :user_id, presence: true
     validates :time, presence: true
 end
--- back/spec/models/study_spec.rb
+++ back/spec/models/study_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Study, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(
+      name: "Test",
+      email: "test@example.com",
+      password: "password",
+    )
+    @skill = Skill.create(
+      name: "Test1",
+      user_id: @user.id,
+    )
+  end
+
+  it "日付、時間、スキルID、ユーザーIDがあれば有効な状態であること" do
+    study = Study.new(
+      date: "2022-12-02",
+      time: "3",
+      skill_id: @skill.id,
+      user_id: @user.id,
+    )
+    expect(study).to be_valid
+  end
+
 end
