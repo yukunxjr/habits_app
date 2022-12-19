b8aea566277c3030111dc48af53807c7aa655307
yukunxjr <yukunxjr@yahoo.co.jp>
2022-12-19 22:30:08 +0900

    メインページの完成

--- front/components/Footer.vue
+++ front/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer padless absolute>
+  <v-footer padless absolute color="white">
     <v-col class="text-center" cols="12">
       {{ new Date().getFullYear() }} - <strong>Habits</strong> -
     </v-col>
